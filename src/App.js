228033513b4f3088bf5b9cd69c76af3d5b71bb85
Robin98123 <robinson1998sena@gmail.com>
2022-12-21 12:19:17 -0500

    se inicia repositorio

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+import Container from './components/Container';
+// import FormTodo from './components/FormTodo';
+// import Checkbox from './components/Checkbox';
+// import Tasklist from './components/TaskList';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Container/>
+    {/* <FormTodo/>
+    <Checkbox/>
+    <Tasklist/> */}
+    </div>
+  );
+}
+
+export default App;
